fde6cd7c3e4b252008664ab2857524004835e9ed
duaaismail24 <doaaee7@gmail.com>
2019-12-04 12:44:14 +0200

    duaa

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var numbers = [[2,5,3,6,8,10],4];
+for (var i = 0; i < numbers[0].length; i++) {
+if (i >= numbers[1]) {
+    return numbers[1];
+    
+    
+}
+
+    else {
+        return "0";
+    }
+}
+
+////////////////////////////////////////////////////////////////
+
+var caps = [];
+var small = ['hello'];
+
+
+///////////////
+var articleEl = document.getElementById ('star');
+
+var pEl = document.creatElemnt ('p')
+articleEl.appendChild.pEl;
+pEl. //////////// I forgot the syntax that we sould put to write text.
+
+var start++;
+
+}
